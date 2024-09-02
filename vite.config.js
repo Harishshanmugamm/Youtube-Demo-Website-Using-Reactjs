406feb7e8bfcee3ba39a27d89899d88afe8a298f
Harishshanmugamm <harishshanmugamm@outlook.com>
2024-09-02 21:37:56 +0530

    new val

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Youtube-Demo-Website-Using-Reactjs/",
   plugins: [react()],
+  
 })
